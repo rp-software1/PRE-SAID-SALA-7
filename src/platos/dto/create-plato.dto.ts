14b5e6d8fa96f3ecc93358d81311bec14539100f
MarcoEC2414 <marcoescalante2414@gmail.com>
2026-05-22 10:41:36 -0500

    PRESAID-D1-B: módulo Platos CRUD completo

---
+++ src/platos/dto/create-plato.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, IsNumber, IsBoolean, IsOptional, MinLength, IsPositive } from 'class-validator';
+
+export class CreatePlatoDto {
+  @IsString()
+  @MinLength(1)
+  nombre: string;
+
+  @IsNumber()
+  @IsPositive()
+  precio: number;
+
+  @IsBoolean()
+  @IsOptional()
+  disponible?: boolean;
+}
